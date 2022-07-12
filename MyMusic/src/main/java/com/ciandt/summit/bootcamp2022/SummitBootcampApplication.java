b3caf7be9d0bfa1bbdf0714b17f8f8dac9104ee7
GustavoJochem <gustavo.jochem14@hotmail.com>
2022-07-12 15:20:30 -0300

    [FEAT] Autenticacao e seguranca

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/SummitBootcampApplication.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/SummitBootcampApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SummitBootcampApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SummitBootcampApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SummitBootcampApplication.class, args);
+    }
 
 }
