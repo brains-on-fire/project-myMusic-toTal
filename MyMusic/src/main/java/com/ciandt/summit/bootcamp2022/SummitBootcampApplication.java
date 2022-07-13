6857556c0ee0a7aca6bdc5788b53590a18dba8b7
Renan de Lima Gomes <renanlg@ciandt.com>
2022-07-13 11:46:50 -0300

    [FIX]

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/SummitBootcampApplication.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/SummitBootcampApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SummitBootcampApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SummitBootcampApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SummitBootcampApplication.class, args);
+    }
+
 
 }
