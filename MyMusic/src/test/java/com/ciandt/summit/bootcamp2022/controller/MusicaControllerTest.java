4a18d73bdb6dea974edcd0f32265d44532bec2b3
Paulo Grillo <paulodigitalart@gmail.com>
2022-07-18 16:31:21 -0300

    [TEST] Add JUnit MusicaControllerTest

---
+++ MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/controller/MusicaControllerTest.java
@@ -0,0 +1,61 @@
+package com.ciandt.summit.bootcamp2022.controller;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class MusicaControllerTest {
+
+    @Autowired
+    MusicaController musicaController;
+
+    @Test
+    @Order(1)
+    @DisplayName("Busca musica > 3 caracteres")
+    public void deveRetornarMusicas(){
+        ResponseEntity<Object> result = musicaController.getByNameOrArtist("The");
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("Busca musica com parametro vazio")
+    public void deveRetornarBadRequest(){
+        ResponseEntity<Object> result = musicaController.getByNameOrArtist("");
+        assertEquals(HttpStatus.BAD_REQUEST, result.getStatusCode());
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Busca musica < 3 caracteres")
+    public void deveRetornarErroNaRequisicao(){
+        ResponseEntity<Object> result = musicaController.getByNameOrArtist("th");
+        assertEquals(HttpStatus.BAD_REQUEST, result.getStatusCode());
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Busca que retorna vazio")
+    public void deveRetornarVazio(){
+        ResponseEntity<Object> result = musicaController.getByNameOrArtist("tasdad");
+        assertEquals(HttpStatus.NO_CONTENT, result.getStatusCode());
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Busca INSENSITIVE LETTER")
+    public void deveRetornarResultadoNormal(){
+        ResponseEntity<Object> result = musicaController.getByNameOrArtist("THE");
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+    }
+
+}
